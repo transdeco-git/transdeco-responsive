f139d89eb6ae56b1859eb18995ac3c42c8c904d8
transdeco-git <info@transdeco.fi>
2021-02-11 22:40:21 +0200

    Update

--- pub.sh
+++ pub.sh
@@ -2,6 +2,7 @@
 
 cd ~/GIT/transdeco-git.github.io
 git checkout main
+git pull https://github.com/transdeco-git/transdeco-git.github.io.git main
 git add -A
 git commit -am "Update"
 git push https://github.com/transdeco-git/transdeco-git.github.io.git main
