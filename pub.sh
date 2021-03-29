abfd6acd1c89be51f1ec8c9cd0972d15a2e26ecc
transdeco-git <info@transdeco.fi>
2021-03-29 21:42:58 +0300

    Update

--- pub.sh
+++ pub.sh
@@ -6,12 +6,12 @@ git pull https://github.com/transdeco-git/transdeco-git.github.io.git main
 git add -A
 git commit -am "Update"
 git push https://github.com/transdeco-git/transdeco-git.github.io.git main
-rm -rf ./_site
-git clone -b gh-pages https://github.com/transdeco-git/transdeco-git.github.io.git _site
-bundle exec jekyll build
-cd _site
-git add -A
-git commit -am "Update"
-git push https://github.com/transdeco-git/transdeco-git.github.io.git gh-pages
-cd ..
-git push --dry-run
+#rm -rf ./_site
+#git clone -b gh-pages https://github.com/transdeco-git/transdeco-git.github.io.git _site
+#bundle exec jekyll build
+#cd _site
+#git add -A
+#git commit -am "Update"
+#git push https://github.com/transdeco-git/transdeco-git.github.io.git gh-pages
+#cd ..
+#git push --dry-run
